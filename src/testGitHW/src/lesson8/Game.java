80987834dc87ee19c8332522abaebbbebe010f60
Ilia Belyaev <ilya.belyaev.95@list.ru>
2021-01-20 13:14:48 +0300

    Прошла лекция 8

---
+++ src/testGitHW/src/lesson8/Game.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+/**
+ * Created by Aleksandr Gladkov [Anticisco]
+ * Date: 11.01.2021
+ */
+
+public class Game {
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
